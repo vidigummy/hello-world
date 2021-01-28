d7d3c38653206ab1f7e6a5574b5a4bcad249156d
vidigummy <vidigummy@gmail.com>
2021-01-28 17:20:22 +0900

    ejs add

--- hello-world/index.js
+++ hello-world/index.js
@@ -7,7 +7,20 @@ app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
   res.send('Hello World!'); // "Hello World!"를 보냅니다.
 });
 */
+/*
 app.use(express.static(__dirname + '/public'));
+*/
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req, res){
+  res.render('hello', {name:req.params.nameParam});
+});
 
 var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
 app.listen(port, function(){ // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
